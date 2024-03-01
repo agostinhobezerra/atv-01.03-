4bf6372017d63aadf84d2874f51c12b69a260f70
agostinhobezerra <146290206+agostinhobezerra@users.noreply.github.com>
2024-03-01 19:56:53 -0300

    Add files via upload

---
+++ att 01.03.js
@@ -0,0 +1,22 @@
+// exercicio 1
+console.log ("Digite 3 número: ");
+v1 = parseFloat(prompt( "Digite o primeiro número: "));
+v2 = parseFloat(prompt( "Digite o segundo número: "));
+v3 = parseFloat(prompt( "Digite o terceiro número: "));
+console.log ("Numero 1: " + v1 + " Numero 2: " + v2 + " Numero 3: " + v3);
+
+// exercicio 2
+var v1, v2, v3, v4, media;
+console.log ("Digite 3 número: ");
+v1 = parseFloat(prompt( "Digite o primeiro número: "));
+v2 = parseFloat(prompt( "Digite o segundo número: "));
+v3 = parseFloat(prompt( "Digite o terceiro número: "));
+v4 = v1+v2+v3;
+media = v4/3;
+console.log ("A média é: " + media);
+
+// exercicio 3
+var n1 = 15;
+var n2 = 25;
+var n3 = 35;
+console.log("Os números são: " + n1 + " " + n2 + " " + n3);
